fe78bb4ae670aa6530d4dca9ff72aca8bd3eda49
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-10-06 10:47:35 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/note_taker_v2/NoteTakerV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.note_taker_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteTakerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/note_taker_v2/NoteTakerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.note_taker_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
